4fa5c0d12cdc7c8f1567f62506be5f89ad5a1932
Suvan <suvanramani@gmail.com>
2016-11-15 13:48:11 -0800

    Add newTodoVC subclass

---
+++ EveryDo/AddToDoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddToDoViewController.h
+//  EveryDo
+//
+//  Created by Suvan Ramani on 2016-11-15.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddToDoViewController : UIViewController
+
+@end
