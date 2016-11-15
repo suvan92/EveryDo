f84e63c45ab7cf0e4ef87cf2cfbc0df404e2b038
Suvan <suvanramani@gmail.com>
2016-11-15 13:49:58 -0800

    Add newTodoVC outlets

--- EveryDo/AddToDoViewController.m
+++ EveryDo/AddToDoViewController.m
@@ -10,6 +10,11 @@
 
 @interface AddToDoViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *titleTextField;
+@property (weak, nonatomic) IBOutlet UITextField *descriptionTextField;
+@property (weak, nonatomic) IBOutlet UITextField *priorityTextField;
+
+
 @end
 
 @implementation AddToDoViewController
@@ -23,15 +28,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
